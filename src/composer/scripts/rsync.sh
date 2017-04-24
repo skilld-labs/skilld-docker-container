ed5f11b9b31c2011112619bb40e131284a440c23
Alexey Gaydabura <gaydabura@gmail.com>
2017-04-24 19:31:40 +0300

    total rework

--- src/composer/scripts/rsync.sh
+++ src/composer/scripts/rsync.sh
@@ -1,16 +1,13 @@
-#!/bin/bash
-# Sync static files to docroot/files
-SYNC_FOLDERS="core modules profiles themes"
-SYNC_EXTENSIONS="js css svg png jpg jpeg ico woff ttf"
-
-function rsync() {
-
-  for DIR in $SYNC_FOLDERS; do
-    for EXTENSION in $SYNC_EXTENSIONS; do
-      rsync -rm --include "*.$EXTENSION" --include "*/" --exclude "*" $DIR/ docroot/$DIR/
-    done
+#!/bin/sh
+  SYNC_FOLDERS="core modules profiles themes"
+  SYNC_EXTENSIONS="js css svg png jpg jpeg ico woff ttf"
+if [ ! -z "$1" ]; then
+  SYNC_FOLDERS=$(dirname $1 | cut -c3-0)
+  SYNC_FOLDERS=$(dirname $1 | cut -c3-0)
+fi
+for DIR in $SYNC_FOLDERS; do
+  for EXTENSION in $SYNC_EXTENSIONS; do
+    rsync -rm --ignore-existing --recursive --delete $DIR/ docroot/$DIR/
+    rsync -rm --include "*.$EXTENSION" --include "*/" --exclude "*" $DIR/ docroot/$DIR/
   done
-}
-
-rsync
-inotifywait core modules profiles themes -m -r -e modify -e create -e delete -o -d && rsync
+done
