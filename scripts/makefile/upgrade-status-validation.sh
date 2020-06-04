db4076521eecabc367249dd2bc4c0e91c456e04c
Juan Peña <juan3789@gmail.com>
2020-06-04 12:37:27 -0500

    Fix upgrade_status test script

--- scripts/makefile/upgrade-status-validation.sh
+++ scripts/makefile/upgrade-status-validation.sh
@@ -5,15 +5,15 @@ drush en -y upgrade_status
 
 # Search for no issues message
 REPORT=$(drush us-a --all --ignore-contrib --ignore-uninstalled)
-IS_VALID=$(echo "$REPORT" | grep "No known issues found.")
+IS_INVALID=$(echo "$REPORT" | grep "FILE:")
 
-# Exit1 and alert if message not found
-if [ -z "$IS_VALID" ]; then
-  printf "There are \033[1missue(s)\033[0m in Upgrade Status report to fix : \n- Go to \033[1m/admin/reports/upgrade-status\033[0m for more details\n"
-	echo -e "$REPORT"
-	exit 1
-else
+# Exit 1 and alert if at least one file was reported.
+if [ -z "$IS_INVALID" ]; then
 	drush pmu upgrade_status -y
 	echo -e "Status report is valid : No error listed"
 	exit 0
+else
+	printf "There are \033[1missue(s)\033[0m in Upgrade Status report to fix : \n- Go to \033[1m/admin/reports/upgrade-status\033[0m for more details\n"
+	echo -e "$REPORT"
+	exit 1
 fi
