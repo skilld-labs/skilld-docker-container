b2bb44678e39b98dacb595121a9f6ed5f1b75909
David Ferlay <dferlay@skilld.cloud>
2022-01-20 16:13:53 +0100

    Update

--- scripts/makefile/front.mk
+++ scripts/makefile/front.mk
@@ -1,18 +1,22 @@
 FRONT_PORT?=65200
+
+# Used to give a random name to Kubernetes pods executed on the fly by "kubectl run"
 RANDOM_STRING = $(shell cat /dev/urandom | tr -dc 'a-fA-F0-9' | tr '[:upper:]' '[:lower:]' | fold -w 10 | head -n 1)
 
+# Convert list of commands to json array format accepted by "kubectl run --overrides" commands
+jsonarrayconverter = echo -n "${1}" | gojq -cRs 'split(" ")'
+## TODO: curl gojq binary according to os/arch
+
+
 # Execute front container function.
-frontexec = docker run \
-	--rm \
-	--init \
-	-u $(CUID):$(CGID) \
-	-v $(CURDIR)/web/themes/custom/$(THEME_NAME):/app \
-	--workdir /app \
-	$(IMAGE_FRONT) ${1}
+frontexec = kubectl run "$(COMPOSE_PROJECT_NAME)-$(RANDOM_STRING)" --image=$(IMAGE_FRONT) --rm -i --quiet --overrides='{ "kind": "Pod", "apiVersion": "v1", "spec": { "volumes": [ { "name": "host-volume", "hostPath": { "path": "$(CURDIR)/web/themes/custom/$(THEME_NAME)" } } ], "containers": [ { "name": "$(COMPOSE_PROJECT_NAME)-$(RANDOM_STRING)", "image": "$(IMAGE_FRONT)", "command": $(shell $(call jsonarrayconverter,${1})), "workingDir": "/app", "volumeMounts": [ { "name": "host-volume", "mountPath": "/app" } ], "terminationMessagePolicy": "FallbackToLogsOnError", "imagePullPolicy": "IfNotPresent" } ], "restartPolicy": "Never", "securityContext": { "runAsUser": $(CUID), "runAsGroup": $(CGID) } } }'
 
-
+# Execute front container function on localhost:FRONT_PORT. Needed for dynamic storybook
+frontexec-with-port = kubectl run "$(COMPOSE_PROJECT_NAME)-$(RANDOM_STRING)" --image=$(IMAGE_FRONT) --rm -i --overrides='{ "kind": "Pod", "apiVersion": "v1", "spec": { "volumes": [ { "name": "host-volume", "hostPath": { "path": "$(CURDIR)/web/themes/custom/$(THEME_NAME)" } } ], "containers": [ { "name": "$(COMPOSE_PROJECT_NAME)-$(RANDOM_STRING)", "image": "$(IMAGE_FRONT)", "command": $(shell $(call jsonarrayconverter,${1})), "stdin": true, "tty": true, "ports": [ { "containerPort": $(FRONT_PORT), "protocol": "TCP" } ], "workingDir": "/app", "volumeMounts": [ { "name": "host-volume", "mountPath": "/app" } ], "terminationMessagePolicy": "FallbackToLogsOnError", "imagePullPolicy": "IfNotPresent" } ], "restartPolicy": "Never", "securityContext": { "runAsUser": $(CUID), "runAsGroup": $(CGID) } } }'
 
 # Execute front container with TTY. Needed for storybook components creation.
+frontexec-with-interactive = kubectl run "$(COMPOSE_PROJECT_NAME)-$(RANDOM_STRING)" --image=$(IMAGE_FRONT) --rm -i --overrides='{ "kind": "Pod", "apiVersion": "v1", "spec": { "volumes": [ { "name": "host-volume", "hostPath": { "path": "$(CURDIR)/web/themes/custom/$(THEME_NAME)" } } ], "containers": [ { "name": "$(COMPOSE_PROJECT_NAME)-$(RANDOM_STRING)", "image": "$(IMAGE_FRONT)", "command": $(shell $(call jsonarrayconverter,${1})), "stdin": true, "tty": true, "workingDir": "/app", "volumeMounts": [ { "name": "host-volume", "mountPath": "/app" } ], "terminationMessagePolicy": "FallbackToLogsOnError", "imagePullPolicy": "IfNotPresent" } ], "restartPolicy": "Never", "securityContext": { "runAsUser": $(CUID), "runAsGroup": $(CGID) } } }'
+
 
 clear-front:
 	@echo "Clean of node_modules and compiled dist... To skip this action please set CLEAR_FRONT_PACKAGES=no in .env file"
@@ -62,18 +66,6 @@ lint:
 		echo "- Theme directory defined in .env file was not found. Skipping theme linters with fix."; \
 	fi
 
-
-
-# Execute front container function on localhost:FRONT_PORT. Needed for dynamic storybook.
-frontexec-with-port = docker run \
-	--rm \
-	--init \
-	-p $(FRONT_PORT):$(FRONT_PORT) \
-	-u $(CUID):$(CGID) \
-	-v $(CURDIR)/web/themes/custom/$(THEME_NAME):/app \
-	--workdir /app \
-	$(IMAGE_FRONT) ${1}
-
 storybook:
 	@if [ -d $(CURDIR)/web/themes/custom/$(THEME_NAME) ]; then \
 		echo "- Theme directory found. Running dynamic storybook..."; \
@@ -85,9 +77,6 @@ storybook:
 	else \
 		echo "- Theme directory defined in .env file was not found. Skipping dynamic storybook."; \
 	fi
-
-
-
 
 build-storybook:
 	@if [ -d $(CURDIR)/web/themes/custom/$(THEME_NAME) ]; then \
@@ -101,15 +90,6 @@ build-storybook:
 		echo "- Theme directory defined in .env file was not found. Skipping dynamic storybook."; \
 	fi
 
-# Execute front container with TTY. Needed for storybook components creation.
 create-component:
 	@echo "Create component CLI dialog... It assumed that you already have 'make storybook' or 'make build-storybook' finished"
 	$(call frontexec-with-interactive,yarn cc)
-
-
-frontexec-with-interactive = kubectl run "$(COMPOSE_PROJECT_NAME)-$(RANDOM_STRING)" --image=$(IMAGE_FRONT) --rm -i --overrides='{ "kind": "Pod", "apiVersion": "v1", "spec": { "volumes": [ { "name": "host-volume", "hostPath": { "path": "$(CURDIR)/web/themes/custom/$(THEME_NAME)" } } ], "containers": [ { "name": "$(RANDOM_STRING)", "image": "$(IMAGE_FRONT)", "command": $(shell $(call jsonarrayconverter,${1})), "stdin": true, "tty": true, "workingDir": "/app", "volumeMounts": [ { "name": "host-volume", "mountPath": "/app" } ], "terminationMessagePolicy": "FallbackToLogsOnError", "imagePullPolicy": "IfNotPresent" } ], "restartPolicy": "Never", "securityContext": { "runAsUser": $(CUID), "runAsGroup": $(CGID) } } }'
-
-jsonarrayconverter = echo -n "${1}" | gojq -cRs 'split(" ")'
-
-vv:
-	$(call jsonarrayconverter,test string)
