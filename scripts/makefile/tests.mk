1f1a910b55a74a6fef266ab9905188b99eca1e15
Nicolas Guerrier <nguerrier@skilld.cloud>
2023-02-12 10:06:41 +0100

    #436 - Move to compose v2

--- scripts/makefile/tests.mk
+++ scripts/makefile/tests.mk
@@ -202,7 +202,7 @@ newrelic:
 ifdef NEW_RELIC_LICENSE_KEY
 	$(call php-0, /bin/sh ./scripts/makefile/newrelic.sh $(NEW_RELIC_LICENSE_KEY) '$(COMPOSE_PROJECT_NAME)')
 	$(call php, sed -i -e 's/#  <<: \*service-newrelic/  <<: \*service-newrelic/g' docker/docker-compose.override.yml)
-	docker-compose up -d
+	docker compose up -d
 	@echo "NewRelic PHP extension enabled"
 else
 	@echo "NewRelic install skipped as NEW_RELIC_LICENSE_KEY is not set"
