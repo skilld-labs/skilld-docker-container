03c2e71447535f9640aef4018c73d1ba3d42b0a2
Andy Postnikov <apostnikov@gmail.com>
2023-03-09 12:45:56 +0100

    upgrade newrelic to 10.7.0.319 - support PHP 8.2
    https://github.com/newrelic/newrelic-php-agent/releases/tag/v10.7.0.319

--- scripts/makefile/newrelic.sh
+++ scripts/makefile/newrelic.sh
@@ -6,7 +6,7 @@
 PHP_INI_DIR=/etc/php81/conf.d
 
 # get the latest version from https://download.newrelic.com/php_agent/archive/
-NEW_RELIC_AGENT_VERSION="${NEW_RELIC_AGENT_VERSION:-10.5.0.317}"
+NEW_RELIC_AGENT_VERSION="${NEW_RELIC_AGENT_VERSION:-10.7.0.319}"
 # change it to 'linux' if docker image is not based on Alpinelinux
 NEW_RELIC_LINUX=${NEW_RELIC_LINUX:-linux-musl}
 
