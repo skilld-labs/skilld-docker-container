732c0572b508fa35e07f303cda32a6dcc9ce8001
Andy Postnikov <apostnikov@gmail.com>
2023-08-13 14:38:15 +0200

    fix backup options order

--- scripts/makefile/backup.mk
+++ scripts/makefile/backup.mk
@@ -7,5 +7,5 @@ backup_name = $(COMPOSE_PROJECT_NAME)-$(datestamp).tar.gz
 backup:
 	rm -f $(backup_name)
 	$(call php, drush sql-dump --database=default --result-file=../$(mysql_dump_name) --structure-tables-list=cachetags,cache_*,flood,sessions,watchdog)
-	tar -czvf $(backup_name) $(files_dir) $(mysql_dump_name) --exclude=$(files_dir)/translations --exclude=$(files_dir)/js --exclude=$(files_dir)/css --exclude=$(files_dir)/styles --exclude=$(files_dir)/php
+	tar -czvf $(backup_name) --exclude=$(files_dir)/translations --exclude=$(files_dir)/js --exclude=$(files_dir)/css --exclude=$(files_dir)/styles --exclude=$(files_dir)/php $(files_dir) $(mysql_dump_name)
 	rm $(mysql_dump_name)
