4588dabae8a52f0cd7f8a2e756031e6c67215fe1
Andy Postnikov <apostnikov@gmail.com>
2024-05-21 16:05:30 +0200

    upgrade newrelic to 10.21.0.11 - security

--- scripts/makefile/newrelic.sh
+++ scripts/makefile/newrelic.sh
@@ -6,7 +6,7 @@
 PHP_INI_DIR=/etc/php83/conf.d
 
 # get the latest version from https://download.newrelic.com/php_agent/archive/
-NEW_RELIC_AGENT_VERSION="${NEW_RELIC_AGENT_VERSION:-10.20.0.10}"
+NEW_RELIC_AGENT_VERSION="${NEW_RELIC_AGENT_VERSION:-10.21.0.11}"
 # change it to 'linux' if docker image is not based on Alpinelinux
 NEW_RELIC_LINUX=${NEW_RELIC_LINUX:-linux-musl}
 
