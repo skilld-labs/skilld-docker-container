a1cacc8f1cbdeb04ea36aed8839cc21c0c1afeb2
Andy Postnikov <apostnikov@gmail.com>
2024-09-16 17:17:25 +0200

    Use FrankenPHP instead of Unit

--- scripts/makefile/reload.sh
+++ scripts/makefile/reload.sh
@@ -7,18 +7,20 @@ YELLOW='\x1b[33;01m'
 
 set -e
 
-socket='--unix-socket /run/control.unit.sock'
 sapi=$(cat /proc/1/comm)
-file=${1:-/var/lib/unit/conf.json}
 
-if [ $sapi == unitd ]; then
+if [ $sapi = "unitd" ]; then
+	socket='--unix-socket /run/control.unit.sock'
 	if [ -z "$1" ]; then
 		# just reload as no new config passed
 		curl -s -o /dev/null $socket http://localhost/control/applications/drupal/restart
 	else
+		file=${1}/conf.json
 		curl -s -o /dev/null -X PUT --data-binary @$file $socket http://localhost/config
 	fi
-elif [ $sapi == php-fpm* ]; then
+elif [ $sapi = "frankenphp" ]; then
+	frankenphp reload -c ${1:-/etc/caddy}/Caddyfile
+elif echo "$sapi" | grep -q '^php-fpm'; then
 	kill -USR2 1;
 else
 	printf "%b unknown SAPI to restart%b\n" "${RED}" "${NC}" && exit 1
