ed25a7e5cee6205dd122a550946136bfd0cdc1c5
Andy Postnikov <apostnikov@gmail.com>
2026-01-15 10:21:42 +0100

    NR: SF8 support https://github.com/newrelic/newrelic-php-agent/releases/tag/v12.4.0.29

--- scripts/makefile/newrelic.sh
+++ scripts/makefile/newrelic.sh
@@ -6,7 +6,7 @@
 PHP_INI_DIR=/etc/php83/conf.d
 
 # get the latest version from https://download.newrelic.com/php_agent/archive/
-NEW_RELIC_AGENT_VERSION="${NEW_RELIC_AGENT_VERSION:-12.3.0.28}"
+NEW_RELIC_AGENT_VERSION="${NEW_RELIC_AGENT_VERSION:-12.4.0.29}"
 # change it to 'linux' if docker image is not based on Alpinelinux
 NEW_RELIC_LINUX=${NEW_RELIC_LINUX:-linux-musl}
 
