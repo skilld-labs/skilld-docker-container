039069da6a239294d03e5f9579498b69ed150d3f
Andy Postnikov <apostnikov@gmail.com>
2026-02-11 00:23:07 +0100

    NR: security https://github.com/newrelic/newrelic-php-agent/releases/tag/v12.5.0.30

--- scripts/makefile/newrelic.sh
+++ scripts/makefile/newrelic.sh
@@ -6,7 +6,7 @@
 PHP_INI_DIR=/etc/php83/conf.d
 
 # get the latest version from https://download.newrelic.com/php_agent/archive/
-NEW_RELIC_AGENT_VERSION="${NEW_RELIC_AGENT_VERSION:-12.4.0.29}"
+NEW_RELIC_AGENT_VERSION="${NEW_RELIC_AGENT_VERSION:-12.5.0.30}"
 # change it to 'linux' if docker image is not based on Alpinelinux
 NEW_RELIC_LINUX=${NEW_RELIC_LINUX:-linux-musl}
 
