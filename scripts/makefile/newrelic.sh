1f2316e1c4e44da1555675a1a28864d80e6d9ac7
Andy Postnikov <apostnikov@gmail.com>
2026-05-15 15:47:50 +0200

    NR: https://github.com/newrelic/newrelic-php-agent/releases/tag/v12.7.0.36

--- scripts/makefile/newrelic.sh
+++ scripts/makefile/newrelic.sh
@@ -6,7 +6,7 @@
 PHP_INI_DIR=/etc/php83/conf.d
 
 # get the latest version from https://download.newrelic.com/php_agent/archive/
-NEW_RELIC_AGENT_VERSION="${NEW_RELIC_AGENT_VERSION:-12.6.0.34}"
+NEW_RELIC_AGENT_VERSION="${NEW_RELIC_AGENT_VERSION:-12.7.0.36}"
 # change it to 'linux' if docker image is not based on Alpinelinux
 NEW_RELIC_LINUX=${NEW_RELIC_LINUX:-linux-musl}
 
